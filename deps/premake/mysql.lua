7bf2a104311291f7be40d5af7ed1a54d736272ce
alice <58637860+alicealys@users.noreply.github.com>
2026-07-19 01:31:18 +0200

    mysql linux build

--- deps/premake/mysql.lua
+++ deps/premake/mysql.lua
@@ -1,15 +1,36 @@
-if (os.host() ~= "windows") then
-    print("automatic mysql installation is not supported on your os")
-    return
-end
-
 mysql = {
 	source = path.join(dependencies.basePath, "mysql"),
-	version = "8.1.0",
-	download = "http://cdn.alicent.cat/mysql-9.7.1-winx64.zip",
+	download_win = {
+		url = "http://cdn.alicent.cat/mysql-9.7.1-winx64.zip",
+		folder = "mysql-9.7.1-winx64"
+	},
+	download_linux = {
+		url = "http://cdn.alicent.cat/mysql-9.7.1-linux-glibc2.28-x86_64-minimal.zip",
+		folder = "mysql-9.7.1-linux-glibc2.28-x86_64-minimal"
+	}
 }
 
-function mysql.install()
+function mysql.install_linux()
+	local hfile = io.open(string.format("%s/include/mysql.h", mysql.source), "r")
+	if (hfile) then
+		return
+	end
+
+    os.execute(string.format("mkdir \"%s\" 2> /dev/null", mysql.source))
+
+	local folder = path.join(mysql.source, mysql.download_linux.folder)
+	local archive = path.join(mysql.source, mysql.download_linux.folder .. ".zip")
+
+	print("Downloading MySQL")
+	os.execute(string.format("wget \"%s\" -O \"%s\"", mysql.download_linux.url, archive))
+	os.execute(string.format("unzip \"%s\" -d \"%s\"", archive, mysql.source))
+
+	os.execute(string.format("mv \"%s\"/* \"%s\"", folder, mysql.source))
+	os.execute(string.format("rm \"%s\"", archive))
+	os.execute(string.format("rmdir \"%s\"", folder))
+end
+
+function mysql.install_win()
 	local hfile = io.open(string.format("%s/include/mysql.h", mysql.source), "r")
 	if (hfile) then
 		return
@@ -17,11 +38,11 @@ function mysql.install()
 
     os.execute(string.format("mkdir \"%s\" 2> nul", mysql.source))
 
-	local folder = path.join(mysql.source, "mysql-9.7.1-winx64")
-	local archive = path.join(mysql.source, "mysql-9.7.1-winx64.zip")
+	local folder = path.join(mysql.source, mysql.download_win.folder)
+	local archive = path.join(mysql.source, mysql.download_win.folder .. ".zip")
 
-	print("Downloading MYSQL")
-	os.execute(string.format("curl \"%s\" -L -o \"%s\"", mysql.download, archive))
+	print("Downloading MySQL")
+	os.execute(string.format("curl \"%s\" -L -o \"%s\"", mysql.download_win.url, archive))
 
     os.execute(string.format("powershell -command \"Expand-Archive -Force \\\"%s\\\" \\\"%s\\\"\"", archive, mysql.source))
     os.execute(string.format("powershell -command \"mv \\\"%s/*\\\" \\\"%s\\\"\"", folder, mysql.source))
@@ -29,8 +50,29 @@ function mysql.install()
     os.execute(string.format("rmdir \"%s\"", folder))
 end
 
+function mysql.install()
+	if (os.host() == "windows") then
+		return mysql.install_win()
+	elseif (os.istarget("linux")) then
+		mysql.install_linux()
+	else
+		error("automatic MySQL installation is not supported on your os")
+	end
+end
+
 function mysql.import()
-	linkoptions {"/DELAYLOAD:libmysql.dll"}
+	if (os.host() == "windows") then
+		linkoptions {"/DELAYLOAD:libmysql.dll"}
+	else
+		libdirs {
+			path.join(mysql.source, "lib")
+		}
+
+		links {
+			"mysqlclient"
+		}
+	end
+
 	defines {"MYSQL_SUPPORTED"}
 
 	mysql.install()
